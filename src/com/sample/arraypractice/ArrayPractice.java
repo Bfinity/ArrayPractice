fd602a2010bd7fade6a01d37b0b3428add6b5730
Brandy Foster <brandy.foster@detroitlabs.com>
2014-09-18 16:10:19 -0400

    added method to count thru loop and calculate average

--- src/com/sample/arraypractice/ArrayPractice.java
+++ src/com/sample/arraypractice/ArrayPractice.java
@@ -4,7 +4,28 @@ package com.sample.arraypractice;
  * Created by BFineRocks on 9/18/14.
  */
 public class ArrayPractice {
+    public static double[] dailyAverageTemp = {98.5, 99.2, 97.8, 99.0, 98.8, 97.6, 97.4};
+
     public static void main(String[] args) {
-        double[] dailyAverageTemp = {98.5, 99.2, 97.8, 99.0, 98.8, 97.6, 97.4};
+
+       
+
+    }
+
+    public static double printOutDailyAverageTemps()
+    {
+        double sum = 0;
+        int count = 0;
+        double weekAverage = 0;
+        for(double temps: dailyAverageTemp)
+        {
+            System.out.println(temps);
+            sum = sum + temps;
+            count++;
+        }
+        weekAverage = sum/count;
+
+        return weekAverage;
+
     }
 }
