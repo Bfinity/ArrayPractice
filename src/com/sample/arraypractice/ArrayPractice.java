cf4f0990aa39bf48eaaf22491c3a7f301b7785b2
Brandy Foster <brandy.foster@detroitlabs.com>
2014-09-18 15:59:26 -0400

    created array list with daily temps initialized.

---
+++ src/com/sample/arraypractice/ArrayPractice.java
@@ -0,0 +1,10 @@
+package com.sample.arraypractice;
+
+/**
+ * Created by BFineRocks on 9/18/14.
+ */
+public class ArrayPractice {
+    public static void main(String[] args) {
+        double[] dailyAverageTemp = {98.5, 99.2, 97.8, 99.0, 98.8, 97.6, 97.4};
+    }
+}
